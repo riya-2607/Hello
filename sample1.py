49c4c3d7c150f623e768498347de6eb797ccad4b
riya-2607 <riyaxavier123@gmail.com>
2023-01-31 19:20:50 -0500

    Update sample1.py

--- sample1.py
+++ sample1.py
@@ -1,3 +1,6 @@
+#Python program to iterate the given list of numbers and print only those numbers which are divisible by 5
+
+
 num_list = [10, 20, 33, 46, 55]
 print("Given list:", num_list)
 print('Divisible by 5:')
